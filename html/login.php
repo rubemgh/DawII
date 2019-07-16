af8b84821bfcba1b5d7f3bc2ca17dc7ac1dc9d25
Rubem <rubemgh23@gmail.com>
2019-07-16 02:03:35 -0300

    finalizando administrador

--- html/login.php
+++ html/login.php
@@ -3,14 +3,15 @@
     
 	
 	<body>
-    <h1 style="background-color:black;color:#F2CD13; text-align: center;">Login</h1>
-	<form action="login_admok.php" method="post" style="border:2px solid black;background-color:#F2CD13; text-align: center;">
-    <center>
-    <img src="../img/barber.jpg" width=120 height=80><br><br>
-      <label>Email</label><input type="text" name="email"/><br><br>
-      <label>Senha</label><input type="password" name="senha"/><br><br>
-     
-      <input type="submit"/>
+    <h1 style="background-color:black;color:#F2CD13; text-align: center; padding-bottom: 40px;  margin-left: -150px;">Login</h1>
+	<form action="login_admok.php" method="post" style="border:2px solid black; background-color:#F2CD13; text-align: center; "><br>
+         <img src="../img/barbudo.png" width=270 height=330 style= 'float: left; margin-left: 150px;' ><br><br>
+         <img src="../img/barberia.png" width=340 height=330 style= 'float: right; margin-right: 260px;' ><br><br>
+         <img src="../img/barber.jpg" width=230 height=180 style='float: none; '><br><br> 
+      
+         <label style='float: none; '>Email</label><input type="text" name="email"/><br><br>
+         <label style='float: none;'>Senha</label><input type="password" name="senha"/><br>
+         <br><input type="submit" style='float: none; margin-left:470px;'/>
       <br><br>
     </form>
     
